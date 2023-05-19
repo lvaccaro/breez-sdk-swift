06ec8504ad5b42ae900a21f12787dbc3bd6c45ef
Luca Vaccaro <me@lvaccaro.com>
2023-05-19 20:42:10 +0200

    set min iOS version 14 and update binary link
    Signed-off-by: Luca Vaccaro <me@lvaccaro.com>

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "bindings-swift",
     platforms: [
         .macOS(.v12),
-        .iOS(.v15),
+        .iOS(.v14),
     ],
     products: [
         .library(name: "BreezSDK", targets: ["breez_sdkFFI", "BreezSDK"]),
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
     ],
     targets: [
-        .binaryTarget(name: "breez_sdkFFI", url: "https://github.com/breez/breez-sdk-swift/releases/download/0.0.4/breez_sdkFFI.xcframework.zip", checksum: "65fadf991e17f0a76f8f843f63ad8af806ccf91181318e6785fa4dda53481fb4"),
+        .binaryTarget(name: "breez_sdkFFI", url: "https://github.com/lvaccaro/breez-sdk-swift/releases/download/0.0.4/breez_sdkFFI.xcframework.zip", checksum: "a16bf6ca5b3bbe6bc96e1a7037877028e791f701e00512259b0908860f31247f"),
         .target(name: "BreezSDK", dependencies: ["breez_sdkFFI"]),
     ]
 )
